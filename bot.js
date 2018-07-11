f9feee5c02538b21d052ca3a525d017090b5486a
Jazzy <41112271+jazzyd6099@users.noreply.github.com>
2018-07-11 10:55:15 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,9 @@ client.on('ready', () => {
 client.on("message", (message) => {
   if (message.content.startsWith("Todoroki")) {
     message.channel.send("Yes? I heard my name.");
-  }
+  } else
+     if (message.content.startsWith("todoroki")) {
+    message.channel.send("Hello, I heard my name.");
 });
 const prefix = ".";
 client.on("message", (message) => {
@@ -23,6 +25,8 @@ client.on("message", (message) => {
     } else
  if (message.content.startsWith(prefix + "hi todoroki")) {
     message.channel.send("Hello.");
+   if (message.content.startsWith(prefix + "help")) {
+    message.channel.send("Okay, my commands so far are: (.)ping, (.)hi todoroki, todoroki, and Todoroki. More will come soon.");
  
        }
 
